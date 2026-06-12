4e470dbdbbb3eef352e26cde9efffb978478a09b
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-06-12 02:34:48 -0700

    feat(use-cases): Implement get-subscriptions.use-case

--- src/application/use-cases/get-subscriptions.use-case.ts
+++ src/application/use-cases/get-subscriptions.use-case.ts
@@ -1,9 +1,46 @@
-import type { SubscriptionQuery } from '../dtos';
-import type { Subscription } from '../../domain/entities';
-import { NotImplementedError } from '../../domain/errors';
+import { NotFoundError } from "../../domain/errors";
+import type { 
+  AuthenticatedUser, 
+  SubscriptionDetailsOutput, 
+  PaginatedSubscriptionsOutput
+} from "../dtos";
+import type { SubscriptionRepository } from "../ports";
+
+interface GetSubscriptionsInput {
+  currentUser: AuthenticatedUser;
+  page: number;
+  limit: number;
+}
+
+type GetSubscriptionsOutput = 
+  | SubscriptionDetailsOutput
+  | PaginatedSubscriptionsOutput;
 
 export class GetSubscriptionsUseCase {
-  execute(_query: SubscriptionQuery): Promise<Subscription[]> {
-    return Promise.reject(new NotImplementedError('Subscription queries'));
+  constructor(
+    private readonly subscriptionRepository: SubscriptionRepository
+  ) {}
+
+  async execute(
+    { currentUser, page, limit }: GetSubscriptionsInput
+  ): Promise<GetSubscriptionsOutput> {
+    if(currentUser.role === 'ADMIN') {
+      const { items, total } = await this.subscriptionRepository.findAll({ page, limit });
+
+      return { 
+        data: items,
+        total,
+        page,
+        limit
+      }
+    }
+
+    const subscription = await this.subscriptionRepository.findCurrentByUserId(currentUser.id);
+
+    if(!subscription) {
+      throw new NotFoundError('Subscription');
+    }
+
+    return subscription;
   }
 }
