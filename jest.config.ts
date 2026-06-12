e0f01b641a47ea55839708b6c190cf7b815b089b
Martin Espericueta <martin.m.espericueta@gmail.com>
2026-06-12 01:04:11 -0700

    chore: configure 80 percent of global coverage

--- jest.config.ts
+++ jest.config.ts
@@ -5,9 +5,18 @@ const config: Config = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   clearMocks: true,
+  collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/**/*.d.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
